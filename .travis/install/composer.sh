5120195da95459ebd522eda714ec1ea00dcfd2fc
Kevin Herrera <kevin@herrera.io>
2016-03-07 13:43:52 -0800

    Overriding original requirements.

--- .travis/install/composer.sh
+++ .travis/install/composer.sh
@@ -11,10 +11,8 @@ must composer install
 
 # Install specific version of Symfony if specified.
 if [ '' != "$SYMFONY_VERSION" ]; then
-    must composer require --dev --no-update symfony/symfony=$SYMFONY_VERSION
-fi
-
-# Install random_compat on older versions of PHP.
-if [ '7.0' != "$TRAVIS_PHP_VERSION" ]; then
-    must composer require paragonie/random_compat
+    must composer require --dev --no-update \
+        symfony/config=${SYMFONY_VERSION} \
+        symfony/dependency-injection=${SYMFONY_VERSION} \
+        symfony/http-kernel=${SYMFONY_VERSION}
 fi
